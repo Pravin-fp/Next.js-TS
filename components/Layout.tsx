61297ffbaff785b3c0c58ec831b2c1b2d54ac057
Pravin-fp <pravin4fairpay@gmail.com>
2026-02-16 11:54:27 +0530

    Ui changes and add header in form

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,11 +9,12 @@ export default function Layout({ children }: { children: ReactNode }) {
       <Sidebar />
 
       {/* Main content */}
-      <main className="ml-64 min-h-screen p-6 overflow-y-auto">
-        <div className="bg-white/70 backdrop-blur-md rounded-xl border border-emerald-200 p-6">
-          {children}
-        </div>
-      </main>
+      <main className="ml-64 min-h-screen p-6 overflow-y-auto bg-gradient-to-br from-emerald-50 to-teal-100">
+      <div className="bg-white shadow-xl rounded-xl border border-emerald-200 p-6">
+      {children}
+  </div>
+</main>
+
     </>
   );
 }
